877a1c0acd47306f47885cd19caf436f384e8710
Dimitris Michailidis <jimichailidis@gmail.com>
2018-09-27 11:15:57 +0200

    More clean preprocessor

--- week3-4/libs/preprocessor.py
+++ week3-4/libs/preprocessor.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
 
 def remove_hashtags(text):
-    text = re.sub('#\S+', '', text, flags=re.MULTILINE)
+    text = re.sub('#\S+', '', text, flags = re.MULTILINE)
     return text
 
 
@@ -15,23 +15,47 @@ def remove_stopwords(text):
     text = [word.lower() for word in text.split() if word.lower() not in stop_words]
     return " ".join(text)
 
+def remove_hyperlinks(text):
+    text = re.sub(r'http\S+', '', text, flags = re.MULTILINE)
+    return text
+
+def remove_mentions(text):
+    text = re.sub(r'@\S+', '', text, flags = re.MULTILINE)
+    return text
+
+def remove_punctuation(text):
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def remove_numbers(text):
+    text = re.sub(r'^\d+\s|\s\d+\s|\s\d+$', '', text, flags = re.MULTILINE)
+    return text
+
+def remove_words_with_numbers(text):
+    text = re.sub(r'\w*\d\w*', '', text, flags = re.MULTILINE)
+    return text
+
+def remove_ampersand(text):
+    text = re.sub(r'amp', '', text, flags=re.MULTILINE)
+    return text
+
 def preprocess_tweets(tweets):
     # Remove hyperlinks
-    tweets = tweets.str.replace(r'http\S+', '', flags=re.MULTILINE)
+    tweets = tweets.apply(remove_hyperlinks)
     # Remove @mentions
-    tweets = tweets.str.replace(r'@\S+', '', flags=re.MULTILINE)
+    tweets = tweets.apply(remove_mentions)
     # Remove #hashtags 
     tweets = tweets.apply(remove_hashtags)
     # Remove stop words
     tweets = tweets.apply(remove_stopwords)
     # Remove punctuation
-    tweets = tweets.str.translate(str.maketrans('', '', string.punctuation))
+    tweets = tweets.apply(remove_punctuation)
     # Remove numbers
-    tweets = tweets.str.replace(r'^\d+\s|\s\d+\s|\s\d+$', '', flags=re.MULTILINE)
+    tweets = tweets.apply(remove_numbers)
     # Remove words that start with a number. To eliminate cases of 000000in etc.
-    tweets = tweets.str.replace(r'\w*\d\w*', '', flags = re.MULTILINE)
-    ## Remove ampersant (will need to remove other symbols as well, see wordcloud)
-    tweets = tweets.str.replace(r'amp', '', flags=re.MULTILINE)
+    tweets = tweets.apply(remove_words_with_numbers)
+    ## Remove ampersand
+    tweets = tweets.apply(remove_ampersand)
     
     return tweets
     
