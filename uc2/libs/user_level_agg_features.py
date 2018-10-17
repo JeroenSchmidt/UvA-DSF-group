fc955c75e943eb57b40e3b4563ffe46d3869b355
Jeroen Schmidt <jeroen.f.l.schmidt@gmail.com>
2018-10-17 14:36:03 +0200

    added another features

--- uc2/libs/user_level_agg_features.py
+++ uc2/libs/user_level_agg_features.py
@@ -1,11 +1,15 @@
-import pandas as pd
+import pandas as __pd
+
+
+__data_dir = "../../data/Visual_well_being/"
 
 
 def age():
     '''Returns age of individual when they filled in the survay'''
-    survey = pd.read_pickle("data/Visual_well_being/survey.pickle")
-    age = pd.to_datetime(survey.start_q).dt.year - survey.born
-    return age
+    survey = __pd.read_pickle(__data_dir + "survey.pickle")
+    survey["age"] = __pd.to_datetime(survey.start_q).dt.year - survey.born
+    survey = survey[["insta_user_id","age"]]
+    return survey
 
 def instagram_account_stats():
     '''
@@ -15,7 +19,7 @@ def instagram_account_stats():
     * Number of photos
     '''
     
-    image_date = pd.read_pickle("data/Visual_well_being/image_data.pickle")
+    image_date = __pd.read_pickle(__data_dir + "image_data.pickle")
     instagram_account_info = image_date[["user_id","user_followed_by","user_follows","user_posted_photos"]]\
                             .drop_duplicates()
     
