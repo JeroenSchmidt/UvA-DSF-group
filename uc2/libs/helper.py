4d6b7abca22d20ee5a34b04b417d1e18c13f1fa3
xaviml <xaviml.93@gmail.com>
2018-10-22 23:53:29 +0200

    cleaning data procedure after merging

--- uc2/libs/helper.py
+++ uc2/libs/helper.py
@@ -11,3 +11,19 @@ def aggregate():
             df['user_id'] = df['user_id'].astype(int)
             dfs.append(df)
     return reduce(lambda left,right: pd.merge(left,right,on='user_id', how='outer').fillna(0), dfs)
+
+def clean_data_after_merging_features(df):
+    drop_columns = ['A_2', 'N_1', 'P_1', 'E_1', 'A_1', 'H_1', 'M_1', 'R_1', 'M_2', 'E_2',
+                    'H_2', 'P_2', 'N_2', 'A_3', 'N_3', 'E_3', 'H_3', 'R_2', 'M_3', 'R_3',
+                    'P_3', 'N_EMO', 'P_EMO', 'HAP', 'LON',
+                    'index', 'id', 'user_id', 'insta_user_id', 'network_id', 'start_q',
+                    'end_q', 'private_account', 'completed', 'HAP',
+                    'Animal', 'Pet', 'Canine', 'Dog', 'Cat']
+    df = df.drop(drop_columns, axis=1)
+
+    for col in df.columns:
+        if (df[col].dtype.__str__() == 'category'):
+            df[col] = df[col].cat.codes
+
+    df = df.dropna(axis=0) # Dropping rows with nan
+    return df
